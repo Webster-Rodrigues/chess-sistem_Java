85ffc4099cf6496d19c001096076e6b35310e7bd
Webster Diogenes <web.driw12@gmail.com>
2023-06-15 19:52:15 -0300

    BoardException

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -8,6 +8,10 @@ public class Board {
 	
 	
 	public Board(int rows, int columns) {
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Erro ao criar tabuleiro! É necessário ao menos ter 1 coluna e 1 linha.");
+		}
+		
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece [rows] [columns];
@@ -20,34 +24,49 @@ public class Board {
 	}
 
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
-
 	public int getColumns() {
 		return columns;
 	}
 
-
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-	
 	
 	public Piece piece (int rows, int columns) {
+		if(!positionExists(rows, columns)) {
+			throw new BoardException("Posição fora do tabuleiro");
+		}
 		return pieces [rows] [columns];
 	}
 	
 	
 	public Piece piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Posição fora do tabuleiro");
+		}
 		return pieces [position.getRow()] [position.getColum()];
 	}
 	
 	
 	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("Já existe uma peça na posição " + position);
+		}
 		pieces[position.getRow()][position.getColum()] = piece;
 		piece.position = position;
+	}
+	
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+	
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColum());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Posição fora do tabuleiro");
+		}
+		return piece(position) != null;
 	}
 	
 
