e3d1aa7a52b3e44bc6f56f696821cf76476b14a0
Webster Diogenes <web.driw12@gmail.com>
2023-06-27 15:29:47 -0300

    Possíveis movimentos do rei

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -18,7 +19,62 @@ public class King extends ChessPiece{
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean [] [] matriz = new boolean [getBoard().getRows()] [ getBoard().getColumns()];
+		
+		Position pos = new Position(0, 0);
+		
+		//above
+		pos.setValues(position.getRow() - 1, position.getColum());
+		if(getBoard().positionExists(pos) && canMove(pos)) {
+			matriz[pos.getRow()][pos.getColum()] = true;
+		}
+		
+		//below
+		pos.setValues(position.getRow() + 1, position.getColum());
+		if(getBoard().positionExists(pos) && canMove(pos)) {
+			matriz[pos.getRow()][pos.getColum()] = true;
+		}
+		
+		//left
+		pos.setValues(position.getRow(), position.getColum() - 1);
+		if(getBoard().positionExists(pos) && canMove(pos)) {
+			matriz[pos.getRow()][pos.getColum()] = true;
+		}
+		
+		//right
+		pos.setValues(position.getRow(), position.getColum() + 1);
+		if(getBoard().positionExists(pos) && canMove(pos)) {
+			matriz[pos.getRow()][pos.getColum()] = true;
+		}
+		
+		//nw
+		pos.setValues(position.getRow() - 1, position.getColum() - 1);
+		if(getBoard().positionExists(pos) && canMove(pos)) {
+			matriz[pos.getRow()][pos.getColum()] = true;
+		}
+		
+		//ne
+		pos.setValues(position.getRow() - 1, position.getColum() + 1);
+		if(getBoard().positionExists(pos) && canMove(pos)) {
+			matriz[pos.getRow()][pos.getColum()] = true;
+		}
+		
+		//sw
+		pos.setValues(position.getRow() + 1, position.getColum() - 1);
+		if(getBoard().positionExists(pos) && canMove(pos)) {
+			matriz[pos.getRow()][pos.getColum()] = true;
+		}
+		
+		//se
+		pos.setValues(position.getRow() + 1, position.getColum() + 1);
+		if(getBoard().positionExists(pos) && canMove(pos)) {
+			matriz[pos.getRow()][pos.getColum()] = true;
+		}
 		return matriz;
+	}
+	
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
 	}
 
 }
